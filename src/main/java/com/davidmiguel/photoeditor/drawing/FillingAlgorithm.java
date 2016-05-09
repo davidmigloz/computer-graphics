848d6b7f6bd86fb67e8c5a158751df3a4e45db48
davidmigloz <me@davidmiguel.com>
2016-05-09 16:03:30 +0200

    Add flood fill 4-way

---
+++ src/main/java/com/davidmiguel/photoeditor/drawing/FillingAlgorithm.java
@@ -0,0 +1,39 @@
+package com.davidmiguel.photoeditor.drawing;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+
+public class FillingAlgorithm {
+	public static void floodFill4(GraphicsContext gc, WritableImage img, int x, int y, Color backgroundColor,
+			Color borderColor) {
+		int width = (int) gc.getCanvas().getWidth();
+		int height = (int) gc.getCanvas().getHeight();
+		boolean[][] painted = new boolean[width][height];
+
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(y);
+		queue.add(x);
+
+		while (!queue.isEmpty()) {
+			int y1 = queue.remove();
+			int x1 = queue.remove();
+
+			if (x1 >= 0 && x1 < width && y1 >= 0 && y1 < height) {
+				Color px = img.getPixelReader().getColor(x1, y1);
+				if (!painted[x1][y1] && !px.equals(borderColor)) {
+					DrawingUtils.putPixel(gc, x1, y1, backgroundColor, 1);
+					img.getPixelWriter().setColor(x1, y1, backgroundColor);
+					painted[x1][y1] = true;
+					queue.add(y1); queue.add(x1 + 1);
+					queue.add(y1); queue.add(x1 - 1);
+					queue.add(y1 + 1); queue.add(x1);
+					queue.add(y1 - 1); queue.add(x1);
+				}
+			}
+		}
+	}
+}
